a99d9787518b9b2b7e6262a30a00747da8dcafd7
FancyUsername <anewone@gmx.de>
2021-02-08 23:07:38 +0100

    1 file modified, 1 added

---
+++ src/app/form-util.ts
@@ -0,0 +1,23 @@
+import { FormControl } from "@angular/forms";
+
+export class FormUtil {
+  config = { emitEvent: false };
+
+  reset(control: FormControl, config?: any) {
+    control.reset(null, { ...config, ...this.config });
+  }
+
+  setValue(control: FormControl, value: any, config?: any) {
+    control.setValue(value, { ...config, ...this.config });
+  }
+
+  execWithoutEmit(fn: () => {}) {
+    let config = this.config;
+    try {
+      this.config = { emitEvent: false };
+      fn();
+    } finally {
+      this.config = config;
+    }
+  }
+}
--- src/app/name-editor/name-editor.component.ts
+++ src/app/name-editor/name-editor.component.ts
@@ -1,22 +1,27 @@
-import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { FormUtil } from "../form-util";
 
 @Component({
-  selector: 'app-name-editor',
-  templateUrl: './name-editor.component.html',
-  styleUrls: ['./name-editor.component.css']
+  selector: "app-name-editor",
+  templateUrl: "./name-editor.component.html",
+  styleUrls: ["./name-editor.component.css"]
 })
-export class NameEditorComponent {
-  name = new FormControl('');
+export class NameEditorComponent implements OnInit {
+  name = new FormControl("");
+  fu = new FormUtil();
 
   updateName() {
-    this.name.setValue('Nancy');
+    this.name.setValue("Nancy");
+  }
+
+  ngOnInit(): void {
+    this.name.valueChanges.subscribe(() => {
+      console.log("value:", this.name.value);
+      this.fu.execWithoutEmit(() => {
+        this.fu.setValue(this.name, "peter");
+        //this.fu.reset(this.name);
+      });
+    });
   }
 }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
