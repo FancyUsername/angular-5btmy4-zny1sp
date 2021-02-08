466026bb7f788ef3f5f8f252127702fedcd75e6c
FancyUsername <anewone@gmx.de>
2021-02-08 21:51:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-5btmy4-zny1sp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-5btmy4-zny1sp)
---
+++ src/app/profile-editor/profile-editor.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
+import { Validators } from "@angular/forms";
+import { FormArray } from "@angular/forms";
+
+import * as _ from "lodash";
+
+@Component({
+  selector: "app-profile-editor",
+  templateUrl: "./profile-editor.component.html",
+  styleUrls: ["./profile-editor.component.css"]
+})
+export class ProfileEditorComponent implements OnInit {
+  aliasSum = 0;
+
+  profileForm = this.fb.group({
+    firstName: ["", Validators.required],
+    lastName: [""],
+    address: this.fb.group({
+      street: [""],
+      city: [""],
+      state: [""],
+      zip: [""]
+    }),
+    aliases: this.fb.array([this.fb.control(0)])
+  });
+
+  get aliases() {
+    return this.profileForm.get("aliases") as FormArray;
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.profileForm.get("aliases").valueChanges.subscribe(() => {
+      console.log("changed", new Date().getTime());
+    });
+    //this.profileForm.valueChanges.subscribe(() => {
+    //this.aliasSum = _.sum(this.profileForm.value.aliases);
+    //console.log("sum:", this.aliasSum);
+    //  console.log("data:", this.profileForm.value);
+    //});
+  }
+
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: "Nancy",
+      address: {
+        street: "123 Drew Street"
+      }
+    });
+  }
+
+  addAlias() {
+    this.aliases.push(this.fb.control(Math.floor(2 + Math.random() * 10)));
+  }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    // console.warn(this.profileForm.value);
+    console.log("submit");
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
