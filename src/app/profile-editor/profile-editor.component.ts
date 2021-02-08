d297c03b7ad76fd3dcc08e24bb34244f6b92ee83
FancyUsername <anewone@gmx.de>
2021-02-08 23:37:06 +0100

    2 files modified

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -72,9 +72,3 @@ export class ProfileEditorComponent implements OnInit {
     console.log("submit");
   }
 }
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
